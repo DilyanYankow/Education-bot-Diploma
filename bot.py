02f2e262935125621b5b11d277deafa065e0866d
DilyanYankow <xdidox3@abv.bg>
2022-04-04 20:58:14 +0300

    bot now can delete messages

--- bot.py
+++ bot.py
@@ -1,34 +1,47 @@
 import random
 import discord
+import os
 from discord.ext import commands
 
 client = commands.Bot(command_prefix='!')
 
-
-@client.event
-async def on_member_join(member):
-    print(f'member has joined the server.')
-
-
-@client.event
-async def on_member_remove(member):
-    print(f'member has left the server.')
-
-
-@client.event
-async def on_ready():
-    print('bot is ready.')
-
+    #Commands
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cog.{extension}')
 
 @client.command()
-async def ping(ctx):
-    await ctx.send(f'Pong! {round(client.latency * 1000)} ms')
-
+async def unload(ctx, extension):
+    client.unload_extension(f'cog.{extension}')
 
 @client.command()
-async def ping(ctx, amount=5):
+async def reload(ctx, extension):
+    client.unload_extension(f'cog.{extension}')
+    client.load_extension(f'cog.{extension}')
+
+@client.command()
+async def purge(ctx, amount=5):
     await ctx.channel.purge(limit=amount+1)
 
+@client.command()
+async def kick(ctx, member : discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+
+@client.command()
+async def ban(ctx, member : discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+    await ctx.send(f'Banned {member.name}#{member.discord}')
+
+@client.command()
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')
+    for ban_entry in banned_users:
+        user = ban_entry.user
+        if (user.name, user.discriminator) == (member.name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.mention}')
+            return
 
 @client.command(aliases=['8ball', 'test'])
 async def _8ball(ctx, *, question):
@@ -56,5 +69,21 @@ async def _8ball(ctx, *, question):
     ]
     await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
 
+    #Events
+@client.event
+async def on_member_join(member):
+    print(f'member has joined the server.')
 
-client.run('OTU4Mjg4MDAwNzc1NzYxOTUw.YkLJPw.IuI25DnjIO2KZhzQPTJSvOx17_k')
+@client.event
+async def on_member_remove(member):
+    print(f'member has left the server.')
+
+@client.event
+async def on_ready():
+    print('bot is ready.')
+
+for filename in os.listdir('./cogs'):   #for each file in /cogs folder
+    if filename.endswith('.py'):
+        client.load_extension((f'cogs.{filename[:-3]}'))
+
+client.run('OTU4Mjg4MDAwNzc1NzYxOTUw.YkLJPw.w1eInjK2VFEYgUWoMb7Fjw-477A')
---
+++ cogs/holder.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+class Holder(commands.Cog):
+    def __init__(self, client):
+        self.client=client
+
+    #Events
+    @commands.Cog.listener()        #function decorator
+    async def on_ready(self):
+        print('Bot is online.')
+
+    #Commands
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send('Pong!')
+
+def setup(client):
+    client.add_cog(Holder(client))
