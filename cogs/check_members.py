1e79c576e3b2c792a0ec6a91445dfd5f9747382e
DilyanYankow <xdidox3@abv.bg>
2022-04-20 14:34:13 +0300

    Sends a list of all users in a channel. "Функцията да отчита студентите които са в разговора и да издава списък с присъстващи"

---
+++ cogs/check_members.py
@@ -0,0 +1,35 @@
+import discord
+from discord import client
+from discord.ext import commands
+
+class Check_members(commands.Cog):
+    def __init__(self, client):
+        self.client=client
+
+    #Events
+    @commands.Cog.listener()        #function decorator
+    async def on_ready(self):
+        print('Bot is online.')
+
+    #Commands
+    @commands.command(aliases=['attendees'])
+    # async def check_members(self, ctx):
+    async def attendees_list(self, ctx):
+        author = ctx.message.author
+        try:
+            user_channel = author.voice.channel
+            members = user_channel.members  # finds members connected to the channel
+            if user_channel:  # If user is in a channel
+                attendees = []  # (list)
+                for member in members:
+                    attendees.append(member.name)
+                await ctx.send(f'{attendees}')
+            else:
+                await ctx.send("Writer is not in a channel")  # If the writer is not in a discord channel
+        except AttributeError:
+            return await ctx.send("User must be in the channel.")  # Error message
+
+
+
+def setup(client):
+    client.add_cog(Check_members(client))
